dba74690d1f85acd33c00dfe3a9dc4eb1d9150b7
Nasim khodapanah <51764413+nasimkhd@users.noreply.github.com>
2021-03-06 23:24:34 -0500

    Update w3p1.c

--- w3p1.c
+++ w3p1.c
@@ -1,12 +1,3 @@
-/*
-    ==================================================
-    Workshop #3 (Part-1):
-    ==================================================
-    Name   : Nasim Khodapanah
-    ID     : 131929200
-    Email  : nkhodapanah@myseneca.ca
-    Section: NGG
-*/
 
 #include <stdio.h>
 #define MIN_YEAR 2010
